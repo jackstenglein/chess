5f46dd8ee69e6f91de05225fc8026a6c1d6c84f7
Jack Stenglein <jackstenglein@gmail.com>
2025-12-11 18:55:45 -0600

    tests: remove profiling from performance test

--- src/__tests__/Performance.test.ts
+++ src/__tests__/Performance.test.ts
@@ -1,34 +1,13 @@
 import fs from 'fs';
-import * as profiler from 'v8-profiler-next';
 import { Chess } from '../Chess';
-
-profiler.setGenerateType(1);
-
-function fail(message: any = '') {
-    expect(`[FAIL] ${message}`.trim()).toBeFalsy();
-}
 
 describe.only('Parsing Performance', () => {
     it('should read massive PGN', () => {
         const data = fs.readFileSync(process.cwd() + '/src/__tests__/data/massive.pgn');
 
-        profiler.startProfiling('massive-pgn', true);
         const start = Date.now();
         new Chess({ pgn: data.toString() });
         const end = Date.now();
-
-        const profile = profiler.stopProfiling('massive-pgn');
-        profile.export(function (error, result: any) {
-            // if it doesn't have the extension .cpuprofile then
-            // chrome's profiler tool won't like it.
-            // examine the profile:
-            //   Navigate to chrome://inspect
-            //   Click Open dedicated DevTools for Node
-            //   Select the profiler tab
-            //   Load your file
-            fs.writeFileSync(`massive-pgn.cpuprofile`, result);
-            profile.delete();
-        });
 
         expect(end - start).toBeLessThanOrEqual(650);
     });
